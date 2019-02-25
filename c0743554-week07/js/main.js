2099dcbae0735555d55c397b1b86b01a232d3600
visualstudio <visualstudio@230-PC14>
2019-02-25 15:31:27 -0500

    object creation

--- c0743554-week07/js/main.js
+++ c0743554-week07/js/main.js
@@ -1,1 +1,31 @@
-﻿// Your code here!
+﻿
+/*function p() {
+sum = 0;
+counter = 117;
+while (counter >= 11) {
+    sum += counter;
+    counter -= 8;
+}
+    return sum;
+    }*/
+var bird = {
+    genus: "corvus",
+    species: "corvax",
+    commonName: "raven",
+    callType: "squawky",
+    maxOffspring: 5,
+    noisy: true,
+    deaddly: false
+};
+
+var bear = {
+    genus: "ursus",
+    species: "arctos",
+    commonName: "brown bear",
+    callType: "rpoar",
+    quote: "",
+    maxOffspring: 3,
+    noisy: true,
+    deadly: false,
+
+};
